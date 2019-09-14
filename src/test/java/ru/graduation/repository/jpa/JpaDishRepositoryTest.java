c9f69541e497e23ad6636eb614a0c1f075bf50d3
virgamot <ipodskochin@mail.ru>
2019-09-14 19:40:58 +0300

    JUnitAdded{  added JUnit dependencies    }

---
+++ src/test/java/ru/graduation/repository/jpa/JpaDishRepositoryTest.java
@@ -0,0 +1,34 @@
+package ru.graduation.repository.jpa;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JpaDishRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
