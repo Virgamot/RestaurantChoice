25174a4a34ad8b7383e6591410801e6e0752b4a3
virgamot <ipodskochin@mail.ru>
2019-10-04 18:31:05 +0300

    RestControllersTest{ }

---
+++ src/test/java/ru/graduation/web/DishRestControllerTest.java
@@ -0,0 +1,24 @@
+package ru.graduation.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.graduation.DishTestData.DISH1_ID;
+
+
+class DishRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = DishRestController.REST_URL + "/";
+
+    @Test
+    void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + DISH1_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
